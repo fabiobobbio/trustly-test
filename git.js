67af8268edff37f31d4a4fa656873daab20c6b8b
Fabio Bobbio <fabio.bobbio@outlook.com>
2020-12-22 20:49:16 -0300

    pending files

--- git.js
+++ git.js
@@ -4,56 +4,56 @@ const splitter = require('stream-splitter')
 const spawn = require('child_process').spawn;
 
 function mountGitUrl(user, repo) {
-return 'https://github.com/' + user + '/' + repo + '.git';
+  return 'https://github.com/' + user + '/' + repo + '.git';
 }
 
 function randomChars(length) {
-var a = 97;
-var result = "";
-for (var i=0; i<length; i++) {
-  result += String.fromCharCode(a + Math.floor(Math.random() * 26));
-}
-return result;
+  var a = 97;
+  var result = "";
+  for (var i=0; i<length; i++) {
+    result += String.fromCharCode(a + Math.floor(Math.random() * 26));
+  }
+  return result;
 }
 
 function buildTmpDirPath() {
-return '/tmp/trustly-test-clone-' + randomChars(8);
+  return '/tmp/trustly-test-clone-' + randomChars(8);
 }
 
 function deleteFolderRecursive(path) {
-if(fs.existsSync(path))  {
-  fs.readdirSync(path).forEach(function(file,index){
-    var curPath = path + "/" + file;
-    if(fs.statSync(curPath).isDirectory()) {
-      deleteFolderRecursive(curPath);
-    } else {
-      fs.unlinkSync(curPath);
-    }
-  });
-  fs.rmdirSync(path);
-}
+  if(fs.existsSync(path))  {
+    fs.readdirSync(path).forEach(function(file,index){
+      var curPath = path + "/" + file;
+      if(fs.statSync(curPath).isDirectory()) {
+        deleteFolderRecursive(curPath);
+      } else {
+        fs.unlinkSync(curPath);
+      }
+    });
+    fs.rmdirSync(path);
+  }
 }
 
 function execGitCloneCmd(gitUrl, clonePath, socket, onSuccess, onError) {
-var proc = spawn('git', ['clone', '-v', gitUrl, clonePath]); 
-var stdoutLines = proc.stdout.pipe(splitter("\n"));
-var stderrLines = proc.stderr.pipe(splitter("\n"));
-stdoutLines.encoding = "utf-8";
-stderrLines.encoding = "utf-8";
-stdoutLines.on("token", function(line) {
-  socket.emit('console-output', line);
-});
-stderrLines.on("token", function(line) {
-  socket.emit('console-output', line);
-});
-proc.on("close", function(code) {
-  if (code != 0) {
-    socket.emit('console-output', "ERROR: git clone failed. Exit code = " + code);
-    onError();
-  } else {
-    onSuccess(clonePath, socket);
-  } 
-});
+  var proc = spawn('git', ['clone', '-v', gitUrl, clonePath]); 
+  var stdoutLines = proc.stdout.pipe(splitter("\n"));
+  var stderrLines = proc.stderr.pipe(splitter("\n"));
+  stdoutLines.encoding = "utf-8";
+  stderrLines.encoding = "utf-8";
+  stdoutLines.on("token", function(line) {
+    socket.emit('console-output', line);
+  });
+  stderrLines.on("token", function(line) {
+    socket.emit('console-output', line);
+  });
+  proc.on("close", function(code) {
+    if (code != 0) {
+      socket.emit('console-output', "ERROR: git clone failed. Exit code = " + code);
+      onError();
+    } else {
+      onSuccess(clonePath, socket);
+    } 
+  });
 }
 
 function cleanup(clonePath, socket) {
